bf4422df832abedfd0f4b8c48e5a2629f05e943d
Bruna Marchezini <bruna.marchezini.reis@gmail.com>
2021-11-30 18:33:53 -0300

    alterado

--- script.js
+++ script.js
@@ -17,54 +17,67 @@ const divQuatro = document.getElementById("div-quatro")
 // let segundaDivTamanho = divOffset.lastElementChild.clientWidth
 // let terceiraDivTamanho = divEnd.lastElementChild.clientWidth
 
+//quantidade dos filhos
 let primeiraDivQuantidade = divStart.childElementCount
 let segundaDivQuantidade = divOffset.childElementCount
 let terceiraDivQuantidade = divEnd.childElementCount
 
 let primeiraDiv = divStart.lastElementChild
 
-let primeiroDisco = false
-let segurando = false
+let mao = undefined
 
-divStart.addEventListener("click", function(event){
-    primeiroDisco = event.currentTarget.lastElementChild
-    console.log(primeiroDisco)
-    divOffset.appendChild(primeiroDisco)
-    // segundaTorre = event.currentTarget
-    // console.log(segundaTorre)
+divStart.addEventListener("click", function(event){ 
+    console.log(event.currentTarget)
 
-    // segundoClick = event.currentTarget.lastElementChild
-    // 
-    if(primeiroDisco !== false){
-        segurando = true
+    const filho = event.currentTarget.lastElementChild
+    console.log(filho)
+    const tamanho = primeiraDiv.clientWidth
+
+    if(filho > tamanho) {
+
     }
-    
 
-    // console.log(primeiroDisco)
-    // console.log(segundoClick)
+    if(mao === undefined) {
+        mao = event.currentTarget.lastElementChild
+    }
+    else {
+        event.currentTarget.appendChild(mao)
+        mao = undefined
+    }
 })
 
+divOffset.addEventListener("click", function(event){ 
+    console.log(event.currentTarget)
 
-// Verificar o estado da variável segurando, qual o valor?
-// Se segundo for falso, verificar se a torre tem algum disco
-// Se segundo for true, comparar disco atual com o último disco da torre clicada
+    const filho = event.currentTarget.lastElementChild
+    console.log(filho)
+    const tamanho = primeiraDiv.clientWidth
 
-
-
-divOffset.addEventListener("click", function(event){
-    event.currentTarget
-    divOffset.appendChild(divStart.lastElementChild)
-    
+    if(mao === undefined) {
+        mao = event.currentTarget.lastElementChild
+    }
+    else {
+        event.currentTarget.appendChild(mao)
+        mao = undefined
+    }
 })
 
-divEnd.addEventListener("click", function(event){
-    event.currentTarget
-    
+divEnd.addEventListener("click", function(event){ 
+    console.log(event.currentTarget)
+
+    const filho = event.currentTarget.lastElementChild
+    console.log(filho)
+    const tamanho = primeiraDiv.clientWidth
+
+    if(mao === undefined) {
+        mao = event.currentTarget.lastElementChild
+    }
+    else {
+        event.currentTarget.appendChild(mao)
+        mao = undefined
+    }
+
 })
-
-
-// let primeiroClick = true
-// let segundoClick = true
 
 
 
