a7e7eeed90f7456e56cfbd121cd5c31e3d9b929e
renandcr <renandamagazine@gmail.com>
2021-12-01 16:52:10 -0300

    Faça alterações na estilização

--- script.js
+++ script.js
@@ -2,33 +2,68 @@
 // clientWidth : dimensão em pixels do primeiro elemento
 // childElementCount : quantidade de elementos filhos
 
-const sectionPai = document.getElementById("section-pai")
-const divStart = document.getElementById("div-start")
-const divOffset = document.getElementById("div-offset")
-const divEnd = document.getElementById("div-end")
+// Elementos criados
+const main = document.querySelector("main")
+const sectionPai = document.createElement("section")
+const divStart = document.createElement("div")
+const divOffset = document.createElement("div")
+const divEnd = document.createElement("div")
 
-const divUm = document.getElementById("div-um")
-const divDois = document.getElementById("div-dois")
-const divTres = document.getElementById("div-tres")
-const divQuatro = document.getElementById("div-quatro")
+const divQuatro = document.createElement("div")
+const divTres = document.createElement("div")
+const divDois = document.createElement("div")
+const divUm = document.createElement("div")
+
+// Atribuição de Classes
+sectionPai.classList.add("section-pai")
+divStart.classList.add("div-start")
+divOffset.classList.add("div-offset")
+divEnd.classList.add("div-end")
+
+divQuatro.classList.add("div-quatro", "bloco")
+divTres.classList.add("div-tres", "bloco")
+divDois.classList.add("div-dois", "bloco")
+divUm.classList.add("div-um", "bloco")
+
+// Atribuição de filhos
+main.appendChild(sectionPai)
+sectionPai.appendChild(divStart)
+sectionPai.appendChild(divOffset)
+sectionPai.appendChild(divEnd)
+
+divStart.appendChild(divQuatro)
+divStart.appendChild(divTres)
+divStart.appendChild(divDois)
+divStart.appendChild(divUm)
+
+
+
+
+// const sectionPai = document.getElementById("section-pai")
+// const divStart = document.getElementById("div-start")
+// const divOffset = document.getElementById("div-offset")
+// const divEnd = document.getElementById("div-end")
+
+// const divUm = document.getElementById("div-um")
+// const divDois = document.getElementById("div-dois")
+// const divTres = document.getElementById("div-tres")
+// const divQuatro = document.getElementById("div-quatro")
 
 
 
 
 //quantidade dos filhos
-let primeiraDivQuantidade = divStart.childElementCount
-let segundaDivQuantidade = divOffset.childElementCount
-let terceiraDivQuantidade = divEnd.childElementCount
+// let primeiraDivQuantidade = divStart.childElementCount
+// let segundaDivQuantidade = divOffset.childElementCount
+// let terceiraDivQuantidade = divEnd.childElementCount
 
 
-let primeiraDiv = divStart.lastElementChild
+// let primeiraDiv = divStart.lastElementChild
 
 let mao = undefined
 
 
 divStart.addEventListener("click", function(event){ 
-    // const filho = event.currentTarget.lastElementChild
-    // const tamanho = primeiraDiv.clientWidth
 
     if(mao === undefined) {
         mao = event.currentTarget.lastElementChild
@@ -49,8 +84,6 @@ divStart.addEventListener("click", function(event){
         
 
 divOffset.addEventListener("click", function(event){ 
-    // const filho = event.currentTarget.lastElementChild
-    // const tamanho = primeiraDiv.clientWidth
 
     if(mao === undefined) {
         mao = event.currentTarget.lastElementChild
@@ -71,8 +104,6 @@ divOffset.addEventListener("click", function(event){
 
 
 divEnd.addEventListener("click", function(event){ 
-    // const filho = event.currentTarget.lastElementChild
-    // const tamanho = primeiraDiv.clientWidth
 
     if(mao === undefined) {
         mao = event.currentTarget.lastElementChild
@@ -90,12 +121,14 @@ divEnd.addEventListener("click", function(event){
     console.log(mao, "offend")
 
     if(divEnd.childElementCount === 4){
-        /// divEnd.appendChild(mao)
 
         let divVitoria = document.createElement("div")
         divVitoria.classList.add("div-vitoria")
         sectionPai.appendChild(divVitoria)
+        divStart.style.display = "none"
+        divOffset.style.display = "none"
         divVitoria.innerText = "Parabéns, você venceu!"
+
     }
 })
 
